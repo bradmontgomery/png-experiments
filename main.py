83e61e2fae9ae84450c4cfe11ff199a70a2178aa
Brad Montgomery <brad@bradmontgomery.net>
2016-07-18 15:07:33 -0500

    adding command-line flags so we can do different thigns

--- main.py
+++ main.py
@@ -16,32 +16,44 @@ def img_data(filename):
     print("\n\n")
 
 
-def duplicate(filename):
-    result_filename = "opt-{}".format(filename)
+def save_copy(filename):
+    new_filename = "opt-{}".format(filename)
 
-    print("Duplicating {} as {}".format(filename, result_filename))
+    print("Duplicating {} as {}".format(filename, new_filename))
     img = Image.open(filename)
     img = img.copy()
 
     # Try setting the compression level:
     # 0 - no compression
     # 9 - max compression
-    #img.save(result_filename, compress_level=9)
+    # img.save(new_filename, compress_level=9)
 
     # Let PIL figure out how to optimize this thing.
     #
     # > If present, instructs the PNG writer to make the output file as small
     # > as possible. This includes extra processing in order to find optimal
     # > encoder settings.
-    img.save(result_filename, optimize=True)
-
-    img_data(result_filename)
+    img.save(new_filename, optimize=True)
+    return new_filename
 
 
 if __name__ == "__main__":
 
-    filename = sys.argv[1] if len(sys.argv) == 2 else None
-    if filename:
+    command = sys.argv[1] if len(sys.argv) == 3 else None
+    filename = sys.argv[2] if len(sys.argv) == 3 else None
+
+    if filename and command == "info":
         print("Opening {}".format(filename))
         img_data(filename)
-        duplicate(filename)
+
+    elif filename and command == "copy":
+        # Info for original
+        img_data(filename)
+
+        # Save a copy
+        filename = save_copy(filename)
+
+        # Info for new file
+        img_data(filename)
+    else:
+        print("\n\nUsage: python main.py <info|copy> <filename>\n")
